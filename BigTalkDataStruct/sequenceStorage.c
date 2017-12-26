f36e06dd48f7e8f5faeeb3754a6c3774bd35a778
clark_linus <haixuanwo@users.noreply.github.com>
2017-12-26 08:44:46 +0800

    dataStruct
    first

---
+++ BigTalkDataStruct/sequenceStorage.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define OK    1
+#define ERROR 0
+#define TRUE  1
+#define FALSE 0
+
+#define MAXSIZE 20
+
+typedef int Status;
+typedef int ElemType;
+
+Status visit(ElemType c)
+{
+	printf("%d ",c);
+	return OK;
+}
+
+typedef struct
+{
+	ElemType data[MAXSIZE];
+	int length;
+}SqList;
+
+Status InitList(SqList *L)
+{
+	L->length = 0;
+	return OK;
+}
+
+Status ListEmpty()
+{
+
+}
+
